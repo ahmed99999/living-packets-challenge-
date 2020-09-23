8a638940ee74597dd4a3f3768d8e4e48f966dd7d
Ahmed Amin Bakhabou <AhmedaminHD@outlook.com>
2020-09-23 12:34:49 +0200

    DEV02: initial commit Slide Component

---
+++ src/Slide/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export interface SlideProps {
+
+}
+
+const Slide: React.FC<SlideProps> = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default Slide;
