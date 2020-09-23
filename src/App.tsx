82ad628848dabef0d8fb57aedd577f16a059137a
Ahmed Amin Bakhabou <AhmedaminHD@outlook.com>
2020-09-23 12:24:43 +0200

    DEV01: inital Slides Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
+      text
     </div>
   );
 }
---
+++ src/Slides/index.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import classes from './index.module.scss';
+
+export interface SlidesProps {
+
+}
+
+const Slides: React.FC<SlidesProps> = () => {
+
+    return (
+        <div></div>
+    );
+}
+
+export default Slides;
