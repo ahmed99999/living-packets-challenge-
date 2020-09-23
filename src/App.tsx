c5186388d7f598fb55e6f74c3171c3cf7f1cc2ec
Ahmed Amin Bakhabou <AhmedaminHD@outlook.com>
2020-09-23 12:48:22 +0200

    DEV03: slides service

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import Slides from './components/Slides';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      text
-    </div>
+    <Slides />
   );
 }
 
---
+++ src/services/Slides_Service/index.ts
@@ -0,0 +1,10 @@
+
+class Slide {
+    constructor(
+        public id: number
+    ) { }
+
+    public static getSlides = () => {
+
+    }
+}
