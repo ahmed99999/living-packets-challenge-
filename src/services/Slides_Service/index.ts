d0377a25c20036ed15f41080cac49490c9a6d03b
Ahmed Amin Bakhabou <AhmedaminHD@outlook.com>
2020-09-23 13:01:57 +0200

    DEV03: adding the slides service

--- src/services/Slides_Service/index.ts
+++ src/services/Slides_Service/index.ts
@@ -1,10 +1,31 @@
+import HttpRequest from '../Http_Service';
 
 class Slide {
     constructor(
         public id: number
     ) { }
 
-    public static getSlides = () => {
+    public static getSlides = async () => {
+        try {
+            const request = new HttpRequest();
+            const { data: slides } = await request.get('/slides');
+            return slides;
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
+    }
 
+    public getSlide = async () => {
+        try {
+            const request = new HttpRequest();
+            const { data: slide } = await request.get(`/slides/${this.id}`);
+            return slide;
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
     }
 }
+
+export default Slide;
